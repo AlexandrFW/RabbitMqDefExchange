e44bccba1ffe9b464b7ae91b6ee536f45ddbcb90
Alexandr <alexandr.kutiev@yandex.ru>
2022-11-02 23:29:40 +0300

    Add project files.

---
+++ PublisherDefExchange/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Default Exchange Publisher");
+
+var counter = 0;
+do
+{
+    int timeToSleep = new Random().Next(1000, 3000);
+    Thread.Sleep(timeToSleep);
+
+    var factory = new ConnectionFactory { HostName = "localhost" };
+    using var connection = factory.CreateConnection();
+    using var channel = connection.CreateModel();
+
+    channel.QueueDeclare(queue: "dev-queue",
+                         durable: false,
+                         exclusive: false,
+                         arguments: null);
+
+    string message = $"Message from Publisher #{counter++}";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "dev-queue",
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($"Message #{counter} is sent to Default exchange");
+}
+while (true);
